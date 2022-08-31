4ad5c6e7be3e310b292f5e3ec7fbd7197243cc77
Hank831209 <hanjhank@gmail.com>
2022-08-31 14:09:37 +0800

    check getTotalSentMessageCount

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def pushMessage(payload):
 
 def getTotalSentMessageCount():
     r = requests.get('https://api.line.me/v2/bot/message/quota/consumption', headers=HEADER)
-    print(r.content)
+    print(r.json())
     return 0
 
 
