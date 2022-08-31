4a44c0ee7c3d19d948f952f315d35de7c89b7291
Hank831209 <hanjhank@gmail.com>
2022-08-31 14:16:00 +0800

    check getTodayCovid19Message

--- app.py
+++ app.py
@@ -243,9 +243,12 @@ def getTotalSentMessageCount():
 
 
 def getTodayCovid19Message():
-    date = ""
-    total_count = 0
-    count = 0  # https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=BGD
+    r = requests.get('https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=BGD',
+                     headers=HEADER)
+    data = r.json()[0]
+    date = data['a04']
+    total_count = date['a05']
+    count = date['a06']
     return F"日期：{date}, 人數：{count}, 確診總人數：{total_count}"
 
 
